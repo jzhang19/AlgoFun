43571a2bb1397012d997c2b118ff54d80bd45965
usalanzhang <usalanzhang@gmail.com>
2022-09-10 13:49:03 -0700

    Update folder stucture Add a new project jms listener

---
+++ spring-boot-projects/spring-boot-jms/src/test/java/com/listener/jms/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listener.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
