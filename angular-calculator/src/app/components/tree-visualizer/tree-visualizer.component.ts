1552e7a7a562829f87886f4a194a01d6f865f5bc
usalanzhang <usalanzhang@gmail.com>
2022-09-18 12:26:23 -0700

    Update the visualizer to display

--- angular-calculator/src/app/components/tree-visualizer/tree-visualizer.component.ts
+++ angular-calculator/src/app/components/tree-visualizer/tree-visualizer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tree-visualizer',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tree-visualizer.component.scss']
 })
 export class TreeVisualizerComponent implements OnInit {
-
+  @Input() syntaxTree: any;
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Child: ", this.syntaxTree);
   }
 
 }
